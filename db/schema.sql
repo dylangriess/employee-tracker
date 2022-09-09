2c9e5a85ec4c0a96c18eca93d3bd6d8e4f59433c
dylangriess <dylangriess@gmail.com>
2022-09-09 09:55:54 -0600

    modified tables for schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,36 +1,33 @@
-
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
 
--- create parent table -> departments
+-- Create Departments Table
 CREATE TABLE departments (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    department_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL
 );
 
--- creates roles table
+-- Create Roles Table
 CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    REFERENCES departments(id)
 );
 
--- creates employee table
-CREATE TABLE employee (
+-- Create Employee Table
+CREATE TABLE employees (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
 	role_id INT NOT NULL,
 	FOREIGN KEY (role_id)
 	REFERENCES roles(id),
-	manager_id INT NOT NULL,
+	manager_id INT,
 	FOREIGN KEY (manager_id)
-	REFERENCES employee(id)
-	ON DELETE SET NULL
+	REFERENCES employees(id)
 );
